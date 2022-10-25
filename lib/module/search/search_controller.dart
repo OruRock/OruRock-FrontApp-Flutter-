8c12c996817c019801ac50db684be8a4299c91e7
whtjtjddn <54929329+whtjtjddn@users.noreply.github.com>
2022-10-25 10:51:01 +0900

    [Enhancement] - 검색 API 기능 추가

--- lib/module/search/search_controller.dart
+++ lib/module/search/search_controller.dart
@@ -1,8 +1,11 @@
 import 'dart:async';
 
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
+import 'package:logger/logger.dart';
 import 'package:oru_rock/function/api_func.dart';
 import 'package:oru_rock/function/map_func.dart';
+import 'package:oru_rock/model/store_model.dart';
 import 'package:oru_rock/module/home/home_controller.dart';
 import 'package:oru_rock/routes.dart';
 
@@ -10,10 +13,14 @@ class SearchController extends GetxController {
   final api = Get.find<ApiFunction>();
   final map = Get.find<MapFunction>();
   final home = Get.find<HomeController>();
+  TextEditingController searchText = TextEditingController();
+  var stores = <StoreModel>[].obs; //store 관리
+  var isLoading = false.obs;
 
   @override
   void onInit() async {
     Get.find<MapFunction>().completer = Completer();
+    stores.value = home.stores.value;
   }
 
   void goMapToSelectedStore(int index) async {
@@ -22,5 +29,22 @@ class SearchController extends GetxController {
     home.showDetailInformation(
         home.markers[index], {"height": null, "width": null});
     map.setCamera(home.markers[index].position!, 18.0);
+  }
+
+  Future<void> search() async {
+    isLoading.value = true;
+    try {
+      final data = {"search_txt": searchText.text};
+      final res = await api.dio.get('/store/list', queryParameters: data);
+
+      final List<dynamic>? storeData = res.data['payload']['result'];
+      if (storeData != null) {
+        stores.value =
+            storeData.map((map) => StoreModel.fromJson(map)).toList();
+      }
+    } catch (e) {
+      Logger().e(e.toString());
+    }
+    isLoading.value = false;
   }
 }
--- lib/module/search/search_page.dart
+++ lib/module/search/search_page.dart
@@ -3,7 +3,6 @@ import 'package:get/get.dart';
 import 'package:oru_rock/constant/style/size.dart';
 import 'package:oru_rock/module/search/search_controller.dart';
 
-
 class Search extends GetView<SearchController> {
   const Search({Key? key}) : super(key: key);
 
@@ -27,100 +26,124 @@ class Search extends GetView<SearchController> {
               const SizedBox(
                 height: GapSize.xSmall,
               ),
-              const TextField(
+              TextField(
+                controller: controller.searchText,
                 decoration: InputDecoration(
                     filled: false,
+                    suffixIcon: IconButton(
+                      onPressed: () {
+                        controller.search();
+                      },
+                      icon: const Icon(
+                        Icons.search,
+                        color: Colors.black,
+                      ),
+                    ),
                     hintText: "암장을 검색해 보세요!",
-                    hintStyle: TextStyle(
+                    hintStyle: const TextStyle(
                         fontSize: FontSize.large,
                         color: Colors.grey,
                         fontFamily: "NotoR"),
-                    enabledBorder:
-                        OutlineInputBorder(borderSide: BorderSide(width: 1.0)),
-                    focusedBorder:
-                        OutlineInputBorder(borderSide: BorderSide(width: 1.0)),
-                    contentPadding: EdgeInsets.symmetric(
+                    enabledBorder: const OutlineInputBorder(
+                        borderSide: BorderSide(width: 1.0)),
+                    focusedBorder: const OutlineInputBorder(
+                        borderSide: BorderSide(width: 1.0)),
+                    contentPadding: const EdgeInsets.symmetric(
                         vertical: GapSize.small, horizontal: GapSize.small)),
-                style:
-                    TextStyle(fontSize: FontSize.large, fontFamily: "NotoR"),
+                style: const TextStyle(
+                    fontSize: FontSize.large, fontFamily: "NotoR"),
               ),
               SizedBox(
                 height: HeightWithRatio.xSmall,
               ),
-              Expanded(
-                child: GridView.builder(
-                  padding: const EdgeInsets.symmetric(
-                      vertical: GapSize.xxSmall, horizontal: GapSize.xxSmall),
-                  itemCount: controller.home.stores.length, //item 개수
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2, //1 개의 행에 보여줄 item 개수
-                    childAspectRatio: 1 / 1.3, //item 의 가로 1, 세로 1.3 의 비율
-                    mainAxisSpacing: 20, //수평 Padding
-                    crossAxisSpacing: 20, //수직 Padding
-                  ),
-                  itemBuilder: (BuildContext context, int index) {
-                    //item 의 반목문 항목 형성
-                    return GestureDetector(
-                      onTap: () {
-                        controller.goMapToSelectedStore(index);
-                      },
-                      child: Column(
-                        children: [
-                          Expanded(
-                            flex: 3,
-                            child: Container(
-                              decoration: BoxDecoration(
-                                  boxShadow: [
-                                    BoxShadow(
-                                      color: Colors.grey.withOpacity(0.2),
-                                      spreadRadius: 3,
-                                      blurRadius: 5,
-                                      offset: const Offset(
-                                          0, 0), // changes position of shadow
-                                    )
-                                  ],
-                                  color: Colors.white,
-                                  borderRadius: const BorderRadius.vertical(
-                                      top: Radius.circular(RadiusSize.large))),
-                              alignment: Alignment.center,
-                              child:
-                                  controller.home.stores[index].image!.isEmpty
-                                      ? Image.asset(
-                                          'asset/image/logo/splash_logo.png')
-                                      : Image.network(controller.home
-                                          .stores[index].image![0].imageUrl!),
-                            ),
+              Obx(
+                () => Expanded(
+                  child: controller.isLoading.value
+                      ? const Center(child: CircularProgressIndicator())
+                      : GridView.builder(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: GapSize.xxSmall,
+                              horizontal: GapSize.xxSmall),
+                          itemCount: controller.stores.length, //item 개수
+                          gridDelegate:
+                              const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2, //1 개의 행에 보여줄 item 개수
+                            childAspectRatio:
+                                1 / 1.3, //item 의 가로 1, 세로 1.3 의 비율
+                            mainAxisSpacing: 20, //수평 Padding
+                            crossAxisSpacing: 20, //수직 Padding
                           ),
-                          Expanded(
-                            flex: 2,
-                            child: Container(
-                              decoration: BoxDecoration(
-                                  boxShadow: [
-                                    BoxShadow(
-                                      color: Colors.grey.withOpacity(0.2),
-                                      spreadRadius: 3,
-                                      blurRadius: 5,
-                                      offset: const Offset(
-                                          0, 5), // changes position of shadow
-                                    )
-                                  ],
-                                  color: Colors.grey[200]!,
-                                  borderRadius: const BorderRadius.vertical(
-                                      bottom:
-                                          Radius.circular(RadiusSize.large))),
-                              alignment: Alignment.center,
-                              child: Text(
-                                "${controller.home.stores[index].stroreName}",
-                                style: const TextStyle(
-                                  fontSize: 20,
-                                ),
+                          itemBuilder: (BuildContext context, int index) {
+                            //item 의 반목문 항목 형성
+                            return GestureDetector(
+                              onTap: () {
+                                controller.goMapToSelectedStore(index);
+                              },
+                              child: Column(
+                                children: [
+                                  Expanded(
+                                    flex: 3,
+                                    child: Container(
+                                      decoration: BoxDecoration(
+                                          boxShadow: [
+                                            BoxShadow(
+                                              color:
+                                                  Colors.grey.withOpacity(0.2),
+                                              spreadRadius: 3,
+                                              blurRadius: 5,
+                                              offset: const Offset(0,
+                                                  0), // changes position of shadow
+                                            )
+                                          ],
+                                          color: Colors.white,
+                                          borderRadius:
+                                              const BorderRadius.vertical(
+                                                  top: Radius.circular(
+                                                      RadiusSize.large))),
+                                      alignment: Alignment.center,
+                                      child: controller
+                                              .stores[index].image!.isEmpty
+                                          ? Image.asset(
+                                              'asset/image/logo/splash_logo.png')
+                                          : Image.network(controller
+                                              .stores[index]
+                                              .image![0]
+                                              .imageUrl!),
+                                    ),
+                                  ),
+                                  Expanded(
+                                    flex: 2,
+                                    child: Container(
+                                      decoration: BoxDecoration(
+                                          boxShadow: [
+                                            BoxShadow(
+                                              color:
+                                                  Colors.grey.withOpacity(0.2),
+                                              spreadRadius: 3,
+                                              blurRadius: 5,
+                                              offset: const Offset(0,
+                                                  5), // changes position of shadow
+                                            )
+                                          ],
+                                          color: Colors.grey[200]!,
+                                          borderRadius:
+                                              const BorderRadius.vertical(
+                                                  bottom: Radius.circular(
+                                                      RadiusSize.large))),
+                                      alignment: Alignment.center,
+                                      child: Text(
+                                        "${controller.stores[index].stroreName}",
+                                        style: const TextStyle(
+                                          fontSize: 20,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ],
                               ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    );
-                  },
+                            );
+                          },
+                        ),
                 ),
               ),
             ],
