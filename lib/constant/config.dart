2044979b6d4ae3716ab755488b04704e203ae4ad
park-cy <cy_park@pntbiz.com>
2022-11-30 14:15:16 +0900

    login api http -> https

--- lib/constant/config.dart
+++ lib/constant/config.dart
@@ -2,7 +2,7 @@ class Config {
   // 임시 key
   static const kakao_native_key =
       "42d5056b9cdc9c3b88e88d8188159cd2"; // develop kakao 에서 앱 등록 후 key 생성 가능
-  static const api_url = "http://www.orurock.shop:8081/api";
+  static const api_url = "https://www.orurock.shop:8081/api";
   static const aos_banner_admob_id = "ca-app-pub-3155451907591531/3093320864";
   static const ios_banner_admob_id = "ca-app-pub-3155451907591531/2053138723";
   static const aos_banner_test_id = "ca-app-pub-3940256099942544/6300978111";
